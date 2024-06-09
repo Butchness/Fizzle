e93c070a1846464e66baa48da639abe8f22e6ad7
Butchness <tlbutcher24@hotmail.com>
2024-06-08 20:34:18 -0700

    Fix pin assignment to ensure on board LED is being toggled correctly.

--- fizzle/src/main.cpp
+++ fizzle/src/main.cpp
@@ -1,12 +1,13 @@
 #include <Arduino.h>
 
 void setup() {
-  pinMode(13, OUTPUT);
+  pinMode(13, OUTPUT); // Use pin number 13 directly
+  Serial.begin(9600);
 }
 
 void loop() {
-  analogWrite(13, HIGH);
-  delay(0.5);
-  analogWrite(13, LOW);
-  delay(0.5);
+  digitalWrite(13, HIGH); // Turn the LED on
+  delay(500);             // Wait for 500 milliseconds
+  digitalWrite(13, LOW);  // Turn the LED off
+  delay(500);             // Wait for 500 milliseconds
 }
