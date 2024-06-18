d5a1f40b999f690b5312451b94863c4fa4d131f1
Butchness <tlbutcher24@hotmail.com>
2024-06-17 17:23:59 -0700

    Add Topigraphical Images for Modeling

--- fizzle/src/main.cpp
+++ fizzle/src/main.cpp
@@ -1,13 +1,13 @@
 #include <Arduino.h>
 
 void setup() {
-  pinMode(13, OUTPUT); // Use pin number 13 directly
+  pinMode(13, OUTPUT);
   Serial.begin(9600);
 }
 
 void loop() {
-  digitalWrite(13, HIGH); // Turn the LED on
-  delay(500);             // Wait for 500 milliseconds
-  digitalWrite(13, LOW);  // Turn the LED off
-  delay(500);             // Wait for 500 milliseconds
+  digitalWrite(13, HIGH); 
+  delay(500);             
+  digitalWrite(13, LOW);  
+  delay(500);             
 }
